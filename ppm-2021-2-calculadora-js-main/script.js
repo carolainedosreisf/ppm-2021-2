e126e7a2b4474c3299fff3766847190866411135
Carolaine dos Reis <caroldosreis97@gmail.com>
2021-08-31 21:02:21 -0300

    31/08/202 parte 1

---
+++ ppm-2021-2-calculadora-js-main/script.js
@@ -0,0 +1,45 @@
+function calcular() {
+   /*
+   * parseFloat - converte uma string em número
+   * variavelNumero.toFixed(2) - mantém apenas dois números após a vírgula
+   */
+   // 1° pegar os números do campo de texto
+   var campo1 = document.querySelector("#numero1");
+   var campo2 = document.querySelector("#numero2");
+   var numero1 = parseFloat(campo1.value);
+   var numero2 = parseFloat(campo2.value);
+   if(isNaN(numero1)) {
+      alert('Número 1 inválido!');
+      return;
+   }
+   if(isNaN(numero2)) {
+      alert('Número 2 inválido!');
+      return;
+   }
+   // 2° pegar a operação
+   var campoOperacao = document.querySelector('#operacao');
+   var operacao = parseInt(campoOperacao.value);
+   // if(isNaN(operacao) || operacao < 1 || operacao > 4) {
+   //    alert('Operação inválida!');
+   //    return;
+   // }
+   // 3° calcular o resultado
+   var resultado;
+   switch(operacao) {
+      case 1: resultado = numero1 + numero2; break;
+      case 2: resultado = numero1 - numero2; break;
+      case 3: resultado = numero1 * numero2; break;
+      case 4:
+         if(numero2 === 0) {
+            alert('O divisor não pode ser 0');
+            return;
+         }
+         resultado = numero1 / numero2;
+         break;
+      default: alert('Operação inválida!'); return;
+   }
+   console.log(resultado);
+   // 4° mostrar o resultado na tela
+   var paragrafo = document.querySelector('#resultado');
+   paragrafo.innerHTML = 'Resultado: ' + resultado;
+} 
